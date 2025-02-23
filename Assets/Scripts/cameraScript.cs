3df72f37632d7d9149b446259be88d10f2703cb2
kwakd <kwakd.oregonstate.edu>
2025-02-23 13:44:59 -0800

    Zoom-In Camera & Bug fix for shaky camera at edge
    added Zoom-in camera with 'Z', will just subtract float values on the orthographic on cinemachine with 'vcam.m_Lens.OrthographicSize'
    
    made it so that the camera would reset to 4.999 when at the edge of the camera.

--- Assets/Scripts/cameraScript.cs
+++ Assets/Scripts/cameraScript.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using TMPro;
 
 public class cameraScript : MonoBehaviour
 {
+    [SerializeField] CinemachineVirtualCamera virtualCamera;
+    private float virtualCameraZoomFloat;
+    public TextMeshProUGUI virtualCameraZoomFloatText;
+
     private gameManager_script gameManagerScript;
+    
 
     private float cameraSpeed = 7.5f;
     // Start is called before the first frame update
     void Start()
     {
         gameManagerScript = FindObjectOfType<gameManager_script>();
+        virtualCameraZoomFloat = 5.5f;
+        virtualCameraZoomFloatText.text = "Zoom: " + virtualCameraZoomFloat.ToString();
+        
     }
 
     // Update is called once per frame
@@ -35,25 +45,47 @@ public class cameraScript : MonoBehaviour
                 transform.Translate(new Vector3(0,cameraSpeed * Time.deltaTime,0));
             }
         }
+        // to make is to that the camera doesnt get stuck on the edges -> itll teleport the camera a very small distance so it wont get stuck
         else
         {
             if(this.gameObject.transform.position.x <= -10)
             {
-                this.gameObject.transform.position = new Vector3(-9.9f, this.gameObject.transform.position.y, -10.0f);
+                this.gameObject.transform.position = new Vector3(-9.999f, this.gameObject.transform.position.y, -10.0f);
             }
             else if(this.gameObject.transform.position.x >= 10)
             {
-                this.gameObject.transform.position = new Vector3(9.9f, this.gameObject.transform.position.y, -10.0f);
+                this.gameObject.transform.position = new Vector3(9.999f, this.gameObject.transform.position.y, -10.0f);
             }
 
             if(this.gameObject.transform.position.y <= -5)
             {
-                this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, -4.9f, -10.0f);
+                this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, -4.999f, -10.0f);
             }
             else if(this.gameObject.transform.position.y >= 5)
             {
-                this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, 4.9f, -10.0f);
+                this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, 4.999f, -10.0f);
             }
         }
+
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            ZoomCameraToggle();
+            //Debug.Log("CAMERA TOGGLE: " + virtualCameraZoomBool);
+        }
+    }
+
+    public void ZoomCameraToggle()
+    {
+        if(virtualCameraZoomFloat > 3.5)
+        {
+            virtualCameraZoomFloat -= 1.0f;
+        }
+        else
+        {
+            virtualCameraZoomFloat = 6.5f;
+        }
+        
+        virtualCamera.m_Lens.OrthographicSize = virtualCameraZoomFloat;
+        virtualCameraZoomFloatText.text = "Zoom: " + virtualCameraZoomFloat.ToString();
     }
 }
